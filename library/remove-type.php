9877c65b21354a77fe6c545aa5fb0e6631febdc4
Jon Brockett <geckomist@gmail.com>
2019-03-12 11:58:25 -0400

    Add library to fix HTML5 warnings

---
+++ library/remove-type.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Remove type= from <script> and <style>
+ *
+ * This will remove these HTML5 markup warnings
+ */
+
+add_action( 'template_redirect', function(){
+    ob_start( function( $tag ){
+        $tag = str_replace( array( 'type="text/javascript"', "type='text/javascript'" ), '', $tag );
+
+        return preg_replace("/type=['\"]text\/(javascript|css)['\"]/", '', $tag);
+    });
+});
