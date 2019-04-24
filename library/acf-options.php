33c1b5308609d2572b8de653eaa3fc78684af632
Jon Brockett <geckomist@gmail.com>
2019-04-24 15:22:59 -0400

    Add default option pages

--- library/acf-options.php
+++ library/acf-options.php
@@ -6,23 +6,23 @@
 if( function_exists('acf_add_options_page') ) {
 
   acf_add_options_page(array(
-  	'page_title' 	=> 'Theme General Settings',
-  	'menu_title'	=> 'Theme Settings',
-  	'menu_slug' 	=> 'theme-general-settings',
+  	'page_title' 	=> 'Site Settings',
+  	'menu_title'	=> 'Site Settings',
+  	'menu_slug' 	=> 'site-settings',
   	'capability'	=> 'edit_posts',
   	'redirect'		=> false
   ));
 
   acf_add_options_sub_page(array(
-  	'page_title' 	=> 'Theme Header Settings',
+  	'page_title' 	=> 'Header Settings',
   	'menu_title'	=> 'Header',
-  	'parent_slug'	=> 'theme-general-settings',
+  	'parent_slug'	=> 'site-settings',
   ));
 
   acf_add_options_sub_page(array(
-  	'page_title' 	=> 'Theme Footer Settings',
+  	'page_title' 	=> 'Footer Settings',
   	'menu_title'	=> 'Footer',
-  	'parent_slug'	=> 'theme-general-settings',
+  	'parent_slug'	=> 'site-settings',
   ));
 
 }
