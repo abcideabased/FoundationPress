252b48595faab88a33d33f5b62d23fa2cd19683e
Jon Brockett <geckomist@gmail.com>
2019-04-30 11:33:34 -0400

    Add breadcrumbs to templates

--- page-templates/page-sidebar-right.php
+++ page-templates/page-sidebar-right.php
@@ -7,6 +7,11 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 <div class="main-container">
 	<div class="main-grid">
+
+		<div class="main-breadcrumb">
+		  <?php site_breadcrumbs(); ?>
+		</div>
+
 		<main id="content" class="main-content" tabindex="-1">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
