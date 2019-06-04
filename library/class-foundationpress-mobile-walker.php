d2c47e98acdb3ba043857949a8ea6ac8a32814b6
Jon Brockett <geckomist@gmail.com>
2019-06-04 09:51:41 -0400

    Add class for parent items to prevent FOUC

--- library/class-foundationpress-mobile-walker.php
+++ library/class-foundationpress-mobile-walker.php
@@ -14,6 +14,7 @@ if ( ! class_exists( 'Foundationpress_Mobile_Walker' ) ) :
 	class Foundationpress_Mobile_Walker extends Walker_Nav_Menu {
 		function start_lvl( &$output, $depth = 0, $args = array() ) {
 			$indent  = str_repeat( "\t", $depth );
+			$output  = preg_replace( "/(.*)(\<li.*?class\=\")([^\"]*)(\".*?)$/", "$1$2$3 is-dropdown-submenu-parent$4", $output );
 			$output .= "\n$indent<ul class=\"vertical nested menu\">\n";
 		}
 	}
--- library/class-foundationpress-top-bar-walker.php
+++ library/class-foundationpress-top-bar-walker.php
@@ -14,6 +14,7 @@ if ( ! class_exists( 'Foundationpress_Top_Bar_Walker' ) ) :
 	class Foundationpress_Top_Bar_Walker extends Walker_Nav_Menu {
 		function start_lvl( &$output, $depth = 0, $args = array() ) {
 			$indent  = str_repeat( "\t", $depth );
+			$output  = preg_replace( "/(.*)(\<li.*?class\=\")([^\"]*)(\".*?)$/", "$1$2$3 is-dropdown-submenu-parent$4", $output );
 			$output .= "\n$indent<ul class=\"dropdown menu vertical\" data-toggle>\n";
 		}
 	}
