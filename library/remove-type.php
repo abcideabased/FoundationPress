74954473ce32df52557fa8a1886fe7715ca05171
Jon Brockett <geckomist@gmail.com>
2019-06-11 11:02:03 -0400

    Better remove of type=

--- library/remove-type.php
+++ library/remove-type.php
@@ -5,10 +5,11 @@
  * This will remove these HTML5 markup warnings
  */
 
-add_action( 'template_redirect', function(){
-    ob_start( function( $tag ){
-        $tag = str_replace( array( 'type="text/javascript"', "type='text/javascript'" ), '', $tag );
-
-        return preg_replace("/type=['\"]text\/(javascript|css)['\"]/", '', $tag);
-    });
-});
+//* Remove type tag from script and style
+add_filter('style_loader_tag', 'remove_type_attr', 10, 2);
+add_filter('script_loader_tag', 'remove_type_attr', 10, 2);
+add_filter('autoptimize_html_after_minify', 'remove_type_attr', 10, 2);
+function remove_type_attr($tag, $handle='')
+{
+    return preg_replace("/type=['\"]text\/(javascript|css)['\"]/", '', $tag);
+}
