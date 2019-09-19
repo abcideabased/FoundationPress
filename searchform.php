f1d32c6cacd28f438232d18ae14d9265b8ba7ee8
Jon Brockett <geckomist@gmail.com>
2019-09-19 09:28:09 -0400

    Add search counter to assign unique IDs when using multiple searches

--- searchform.php
+++ searchform.php
@@ -6,14 +6,22 @@
  * @since FoundationPress 1.0.0
  */
 
+// Assigns unique IDs when multiple search forms are used
+global $search_form_counter;
+if ( ! isset( $search_form_counter ) ) $search_form_counter = 0;
+$form_id = $search_form_counter ? '' : ' id="searchform"';
+$text_id = $search_form_counter ? 's_' . $search_form_counter : 's';
+$submit_id = $search_form_counter ? '' : ' id="searchsubmit"';
+$search_form_counter++;
+
 ?>
 
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get"<?php echo $form_id; ?> class="searchform" action="<?php echo home_url( '/' ); ?>">
 	<div class="input-group">
-		<input type="text" class="input-group-field" value="" name="s" id="s" aria-label="Search" placeholder="<?php
+		<input type="text" class="input-group-field" value="" name="s" id="<?php echo $text_id; ?>" aria-label="Search" placeholder="<?php
 		esc_attr_e( 'Search', 'foundationpress' ); ?>">
 		<div class="input-group-button">
-			<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>" class="button">
+			<input type="submit"<?php echo $submit_id; ?> class="searchsubmit button" value="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>">
 		</div>
 	</div>
 </form>
