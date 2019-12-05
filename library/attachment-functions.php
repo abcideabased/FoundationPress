ebbece4e97a059aa3d0e760e6fd90d9e1a54dafa
jonbrockett <geckomist@gmail.com>
2019-12-05 09:06:55 -0500

    Start dev on converting img to picture

--- library/attachment-functions.php
+++ library/attachment-functions.php
@@ -25,6 +25,16 @@ function get_lazy_class() {
   return 'lazy';
 }
 
+// Use <picture> instead of <img>
+function use_picture() {
+  return false;
+}
+
+// Support .webp
+function support_webp() {
+  return false;
+}
+
 /**
  * Lazy Load Image - Custom wp_get_attachment_image
  */
@@ -530,3 +540,112 @@ function the_thumbnail_bg($size = 'post-thumbnail') {
     echo esc_url( $url );
   }
 }
+
+/**
+ * Get the <img> as <picture>
+ * @return string <picture> tag
+ */
+ function get_the_picture( $attachment_id, $size = 'thumbnail', $icon = false, $attr = '' ) {
+     $html  = '<picture>';
+
+     // Process the image
+     $image = wp_get_attachment_image_src( $attachment_id, $size, $icon );
+     if ( $image ) {
+         list($src, $width, $height) = $image;
+         $hwstring                   = image_hwstring( $width, $height );
+         $size_class                 = $size;
+         if ( is_array( $size_class ) ) {
+             $size_class = join( 'x', $size_class );
+         }
+         $attachment   = get_post( $attachment_id );
+
+         // For use in <img>
+         $default_attr = array(
+             'src'      => $src,
+             'class'    => "attachment-$size_class size-$size_class",
+             'alt'      => trim( strip_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ) ),
+         );
+
+         // Pic attr
+         $pic_attr = '';
+         $pic_attr = wp_parse_args( $attr, $pic_attr );
+
+         // Image attr
+         $attr = wp_parse_args( $attr, $default_attr );
+
+         // Generate 'srcset' and 'sizes' if not already present.
+         if ( empty( $attr['srcset'] ) ) {
+             $image_meta = wp_get_attachment_metadata( $attachment_id );
+
+             if ( is_array( $image_meta ) ) {
+                 $size_array = array( absint( $width ), absint( $height ) );
+                 $srcset     = wp_calculate_image_srcset( $size_array, $src, $image_meta, $attachment_id );
+                 $sizes      = wp_calculate_image_sizes( $size_array, $src, $image_meta, $attachment_id );
+
+                 if ( $srcset && ( $sizes || ! empty( $attr['sizes'] ) ) ) {
+
+                     // Set picture
+                     $pic_attr['srcset'] = $srcset;
+
+                     if ( empty( $pic_attr['media'] ) ) {
+                         $pic_attr['media'] = $sizes;
+                     }
+
+                     // Set img
+                     $attr['srcset'] = $srcset;
+
+                     if ( empty( $attr['sizes'] ) ) {
+                         $attr['sizes'] = $sizes;
+                     }
+                 }
+             }
+         }
+
+         /**
+          * Filters the list of attachment image attributes.
+          *
+          * @since 2.8.0
+          *
+          * @param array        $attr       Attributes for the image markup.
+          * @param WP_Post      $attachment Image attachment post.
+          * @param string|array $size       Requested size. Image size or array of width and height values
+          *                                 (in that order). Default 'thumbnail'.
+          */
+         $pic_attr = apply_filters( 'wp_get_attachment_image_attributes', $pic_attr, $attachment, $size );
+         $pic_attr = array_map( 'esc_attr', $pic_attr );
+         $html .= rtrim( "<source " );
+         foreach ( $pic_attr as $name => $value ) {
+             $html .= " $name=" . '"' . $value . '"';
+         }
+         /*foreach ( $pic_attr as $pic_attr['srcset'] => $value ) {
+             $html .= '<source media="' . $value . '" />';
+         }*/
+         $html .= ' />';
+         // @TODO: Write img srcset into multiple <picture> sources
+
+
+
+
+         /**
+          * Filters the list of attachment image attributes.
+          *
+          * @since 2.8.0
+          *
+          * @param array        $attr       Attributes for the image markup.
+          * @param WP_Post      $attachment Image attachment post.
+          * @param string|array $size       Requested size. Image size or array of width and height values
+          *                                 (in that order). Default 'thumbnail'.
+          */
+         $attr = apply_filters( 'wp_get_attachment_image_attributes', $attr, $attachment, $size );
+         $attr = array_map( 'esc_attr', $attr );
+         $html .= rtrim( "<img $hwstring" );
+         foreach ( $attr as $name => $value ) {
+             $html .= " $name=" . '"' . $value . '"';
+         }
+         $html .= ' />';
+
+     }
+     $html .= '</picture>';
+
+     return $html;
+ }
