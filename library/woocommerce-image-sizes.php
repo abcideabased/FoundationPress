b238f9491c537a1c0a9d12fd1ddc39e07cb318b4
jonbrockett <geckomist@gmail.com>
2019-12-23 12:09:05 -0500

    Set max height for woocommerce images

--- library/woocommerce-image-sizes.php
+++ library/woocommerce-image-sizes.php
@@ -3,21 +3,21 @@
 add_filter( 'woocommerce_get_image_size_gallery_thumbnail', function( $size ) {
     return array(
         'width'  => 150,
-        'height' => 0,
+        'height' => 150,
         'crop'   => false,
     );
 } );
 add_filter( 'woocommerce_get_image_size_thumbnail', function( $size ) {
     return array(
         'width'  => 300,
-        'height' => 0,
+        'height' => 300,
         'crop'   => false,
     );
 } );
 add_filter( 'woocommerce_get_image_size_single', function( $size ) {
     return array(
         'width'  => 1024,
-        'height' => 0,
+        'height' => 1024,
         'crop'   => false,
     );
 } );
